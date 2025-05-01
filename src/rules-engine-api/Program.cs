5e24a77cf4b029b2a99452d2bec752fc65b0bbbb
Renan Pereira <rpereira@glasslewis.com>
2025-05-01 03:43:15 +0100

    http redirect

--- src/rules-engine-api/Program.cs
+++ src/rules-engine-api/Program.cs
@@ -4,17 +4,18 @@ using RulesEnginePro.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
 builder.Services.AddRulesEnginePro();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddGithubAuthentication(builder.Configuration);
 
+// Configure CORS
 var frontendOrigin = builder.Configuration.GetSection("Cors:Urls").Get<string[]>() ?? [];
 Console.WriteLine($"config cors are {string.Join(",", frontendOrigin)}");
 
 builder.Services.AddCors(options =>
 {
-    
     options.AddPolicy("rules-engine-pro", policy =>
     {
         policy.WithOrigins(frontendOrigin)
@@ -25,11 +26,20 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+// Redirect HTTP to HTTPS
+app.UseHttpsRedirection();
+
+// Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// Middleware
 app.UseCors("rules-engine-pro");
 app.UseAuthentication();
 app.UseAuthorization();
+
+// Endpoints
 app.MapWorkflowEndpoints();
 app.MapUserEndpoints();
 
