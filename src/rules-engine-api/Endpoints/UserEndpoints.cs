f734bed7ef4a9a124b523baed0ae7266b305f842
Renan Pereira <rpereira@glasslewis.com>
2025-05-01 21:28:18 +0100

    testing

--- src/rules-engine-api/Endpoints/UserEndpoints.cs
+++ src/rules-engine-api/Endpoints/UserEndpoints.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RulesEnginePro.Models;
 using System.Security.Claims;
 
